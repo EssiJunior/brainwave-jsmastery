61cde3227b0f92637ca30985bfc74e8e564612ca
EssiJunior <nessipjunior@gmail.com>
2024-07-14 22:52:31 +0100

    ...Terminated Benefits section

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Button from "@/components/Button";
 import Section from "@/components/Section";
+import Benefits from "@/containers/Benefits";
 import Hero from "@/containers/Hero";
 import Image from "next/image";
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main className="pt-[4.75rem] lg:pt-[5.75rem] overflow-hidden">
       <Hero />
+      <Benefits />
       <Button >Button</Button>
     </main>
   );
